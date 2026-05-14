d8eddc7a43d3ed68e5fe9572e3cd20f955270795
Dario Ardito <dario.ardito@students.fhnw.ch>
2026-05-14 17:51:15 +0200

    Add Quiz and Question models

--- domain/models.py
+++ domain/models.py
@@ -19,3 +19,23 @@ class Question(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     text: str
     quiz_id: int = Field(foreign_key='quiz.id')
+
+
+    class AnswerOption(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    text: str
+    is_correct: bool
+    question_id: int = Field(foreign_key='question.id')
+class QuizAttempt(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    student_id: int = Field(foreign_key='user.id')
+    quiz_id: int = Field(foreign_key='quiz.id')
+    score: int
+    max_score: int
+    completed_at: datetime = Field(default_factory=datetime.now)
+class StudentAnswer(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    attempt_id: int = Field(foreign_key='quizattempt.id')
+    question_id: int = Field(foreign_key='question.id')
+    selected_answer_option_id: int = Field(foreign_key='answeroption.id')
+    is_correct: bool
