16b484ffe1b9e5795bb616070a280dc5501c5920
Arthur - Saryan <arthur.saryan@students.fhnw.ch>
2026-05-19 00:40:18 +0200

    Add pages/teacher __init__ with imports

--- pages/teacher/__init__.py
+++ pages/teacher/__init__.py
@@ -1,1 +1,6 @@
 """Teacher pages package."""
+
+from pages.teacher.dashboard import teacher_dashboard
+from pages.teacher.quiz_create import quiz_create
+from pages.teacher.quiz_results import quiz_results
+
